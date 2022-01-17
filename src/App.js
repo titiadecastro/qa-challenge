f66547c8bea0ce7aa55556fc174bc40e9c3c329c
Titia de Castro <decastrotitia@gmail.com>
2022-01-18 03:43:30 +0800

    removed extra div

--- src/App.js
+++ src/App.js
@@ -29,47 +29,45 @@ const App = () => {
 
   return (
     <div>
-      <div>
-        {
-          isLoading ? <div>Loading...</div> :
+      {
+        isLoading ? <div>Loading...</div> :
+        <div>
           <div>
-            <div>
-              <p onClick={() => setMode('lyrics')}>Lyrics</p>
-              <p onClick={() => setMode('movies')}>Movies</p>
-            </div>
-            {
-              mode === 'lyrics' ?
-              <>
-                <h1>Search for the lyrics to any song</h1>
-                <input type='text' placeholder='Taylor Swift' value={artist} onChange={e => setArtist(e.target.value)} />
-                <input type='text' placeholder='All Too Well' value={title} onChange={e => setTitle(e.target.value)} />
-                <button onClick={getLyrics}>Search</button>
-                {
-                  lyrics &&
-                  <p style={{'whiteSpace': 'pre'}}>{lyrics}</p>
-                }
-              </>
-              :
-              <>
-                <h1>Search for a movie</h1>
-                <input type='text' placeholder='Movie Title' />
-                <button>Search</button>
-                {
-                  movie.Title &&
-                  <div>
-                    <img src='https://via.placeholder.com/300x450' alt="" />
-                    <p>Title (Year)</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus massa diam, laoreet ac euismod eget, faucibus semper ligula. Praesent sed lacus ac tortor euismod varius. Vivamus sed diam libero. Duis.</p>
-                    <p>Runtime:</p>
-                    <p>Director:</p>
-                    <p>Stars:</p>
-                  </div>
-                }
-              </>
-            }
+            <p onClick={() => setMode('lyrics')}>Lyrics</p>
+            <p onClick={() => setMode('movies')}>Movies</p>
           </div>
-        }
-      </div>
+          {
+            mode === 'lyrics' ?
+            <>
+              <h1>Search for the lyrics to any song</h1>
+              <input type='text' placeholder='Taylor Swift' value={artist} onChange={e => setArtist(e.target.value)} />
+              <input type='text' placeholder='All Too Well' value={title} onChange={e => setTitle(e.target.value)} />
+              <button onClick={getLyrics}>Search</button>
+              {
+                lyrics &&
+                <p style={{'whiteSpace': 'pre'}}>{lyrics}</p>
+              }
+            </>
+            :
+            <>
+              <h1>Search for a movie</h1>
+              <input type='text' placeholder='Movie Title' />
+              <button>Search</button>
+              {
+                movie.Title &&
+                <div>
+                  <img src='https://via.placeholder.com/300x450' alt="" />
+                  <p>Title (Year)</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus massa diam, laoreet ac euismod eget, faucibus semper ligula. Praesent sed lacus ac tortor euismod varius. Vivamus sed diam libero. Duis.</p>
+                  <p>Runtime:</p>
+                  <p>Director:</p>
+                  <p>Stars:</p>
+                </div>
+              }
+            </>
+          }
+        </div>
+      }
     </div>
   )
 }
