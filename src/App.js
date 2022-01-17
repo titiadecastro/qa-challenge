6ae546f0b32ff2d918547c7ed0d9bdbde77435bb
Titia de Castro <decastrotitia@gmail.com>
2022-01-18 02:34:19 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import axios from 'axios'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [artist, setArtist] = useState('')
+  const [title, setTitle] = useState('')
+  const [lyrics, setLyrics] = useState('')
+
+  const getLyrics = async () => {
+    setIsLoading(true)
+    try {
+      const lyricsData = await axios({
+        url: `https://api.lyrics.ovh/v1/${artist}/${title}`,
+        method: 'get'
+      })
+      console.log(lyricsData.data.lyrics)
+      setLyrics(lyricsData.data.lyrics)
+      setIsLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        {
+          isLoading ? <div>Loading...</div> :
+          <>
+            <h1>Search for the lyrics to any song</h1>
+            <input type='text' placeholder='Taylor Swift' value={artist} onChange={e => setArtist(e.target.value)} />
+            <input type='text' placeholder='All Too Well' value={title} onChange={e => setTitle(e.target.value)} />
+            <button className='button' onClick={getLyrics}>Search</button>
+            {
+              lyrics &&
+              <p style={{'whiteSpace': 'pre'}}>{lyrics}</p>
+            }
+          </>
+        }
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
