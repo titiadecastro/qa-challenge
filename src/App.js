f30fe4221d111918842c68791afec6fc59d49c63
Titia de Castro <decastrotitia@gmail.com>
2022-01-18 03:08:08 +0800

    added movie searcher

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import axios from 'axios'
 import './App.css'
 
 const App = () => {
+  const [mode, setMode] = useState('lyrics')
   const [isLoading, setIsLoading] = useState(false)
   const [artist, setArtist] = useState('')
   const [title, setTitle] = useState('')
   const [lyrics, setLyrics] = useState('')
+
+  const [search, setSearch] = useState('')
+  const [movie, setMovie] = useState({})
 
   const getLyrics = async () => {
     setIsLoading(true)
@@ -15,7 +19,7 @@ const App = () => {
         url: `https://api.lyrics.ovh/v1/${artist}/${title}`,
         method: 'get'
       })
-      console.log(lyricsData.data.lyrics)
+      console.log(lyricsData.data)
       setLyrics(lyricsData.data.lyrics)
       setIsLoading(false)
     } catch (error) {
@@ -25,19 +29,45 @@ const App = () => {
 
   return (
     <div>
-      <div className='container'>
+      <div>
         {
           isLoading ? <div>Loading...</div> :
-          <>
-            <h1>Search for the lyrics to any song</h1>
-            <input type='text' placeholder='Taylor Swift' value={artist} onChange={e => setArtist(e.target.value)} />
-            <input type='text' placeholder='All Too Well' value={title} onChange={e => setTitle(e.target.value)} />
-            <button className='button' onClick={getLyrics}>Search</button>
+          <div>
+            <div>
+              <p onClick={() => setMode('lyrics')}>Lyrics</p>
+              <p onClick={() => setMode('movies')}>Movies</p>
+            </div>
             {
-              lyrics &&
-              <p style={{'whiteSpace': 'pre'}}>{lyrics}</p>
+              mode === 'lyrics' ?
+              <>
+                <h1>Search for the lyrics to any song</h1>
+                <input type='text' placeholder='Taylor Swift' value={artist} onChange={e => setArtist(e.target.value)} />
+                <input type='text' placeholder='All Too Well' value={title} onChange={e => setTitle(e.target.value)} />
+                <button onClick={getLyrics}>Search</button>
+                {
+                  lyrics &&
+                  <p style={{'whiteSpace': 'pre'}}>{lyrics}</p>
+                }
+              </>
+              :
+              <>
+                <h1>Search for a movie</h1>
+                <input type='text' placeholder='Movie Title' />
+                <button>Search</button>
+                {
+                  movie.Title &&
+                  <div>
+                    <img src='https://via.placeholder.com/300x450' alt="" />
+                    <p>Title (Year)</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus massa diam, laoreet ac euismod eget, faucibus semper ligula. Praesent sed lacus ac tortor euismod varius. Vivamus sed diam libero. Duis.</p>
+                    <p>Runtime:</p>
+                    <p>Director:</p>
+                    <p>Stars:</p>
+                  </div>
+                }
+              </>
             }
-          </>
+          </div>
         }
       </div>
     </div>
