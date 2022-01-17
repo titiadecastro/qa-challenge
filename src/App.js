26b9649229aea0cbb446fb111efc18eba128cc03
Titia de Castro <decastrotitia@gmail.com>
2022-01-18 03:48:39 +0800

    loader and divs

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const App = () => {
   return (
     <div>
       {
-        isLoading ? <div>Loading...</div> :
+        isLoading ? <div class="loader loader--style3" title="2"><svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="40px" height="40px" viewBox="0 0 50 50" xmlSpace="preserve"><path fill="#000" d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z"><animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" dur="0.6s" repeatCount="indefinite"/></path></svg></div> :
         <div>
           <div>
             <p onClick={() => setMode('lyrics')}>Lyrics</p>
@@ -38,7 +38,7 @@ const App = () => {
           </div>
           {
             mode === 'lyrics' ?
-            <>
+            <div>
               <h1>Search for the lyrics to any song</h1>
               <input type='text' placeholder='Taylor Swift' value={artist} onChange={e => setArtist(e.target.value)} />
               <input type='text' placeholder='All Too Well' value={title} onChange={e => setTitle(e.target.value)} />
@@ -47,9 +47,9 @@ const App = () => {
                 lyrics &&
                 <p style={{'whiteSpace': 'pre'}}>{lyrics}</p>
               }
-            </>
+            </div>
             :
-            <>
+            <div>
               <h1>Search for a movie</h1>
               <input type='text' placeholder='Movie Title' />
               <button>Search</button>
@@ -64,7 +64,7 @@ const App = () => {
                   <p>Stars:</p>
                 </div>
               }
-            </>
+            </div>
           }
         </div>
       }
