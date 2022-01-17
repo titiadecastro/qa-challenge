1aee73ac90d06bc176892087daf79b781302df6a
Titia de Castro <decastrotitia@gmail.com>
2022-01-18 03:43:03 +0800

    added styles

--- src/App.js
+++ src/App.js
@@ -28,48 +28,46 @@ const App = () => {
   }
 
   return (
-    <div>
-      <div>
-        {
-          isLoading ? <div>Loading...</div> :
-          <div>
-            <div>
-              <p onClick={() => setMode('lyrics')}>Lyrics</p>
-              <p onClick={() => setMode('movies')}>Movies</p>
-            </div>
-            {
-              mode === 'lyrics' ?
-              <>
-                <h1>Search for the lyrics to any song</h1>
-                <input type='text' placeholder='Taylor Swift' value={artist} onChange={e => setArtist(e.target.value)} />
-                <input type='text' placeholder='All Too Well' value={title} onChange={e => setTitle(e.target.value)} />
-                <button onClick={getLyrics}>Search</button>
-                {
-                  lyrics &&
-                  <p style={{'whiteSpace': 'pre'}}>{lyrics}</p>
-                }
-              </>
-              :
-              <>
-                <h1>Search for a movie</h1>
-                <input type='text' placeholder='Movie Title' />
-                <button>Search</button>
-                {
-                  movie.Title &&
-                  <div>
-                    <img src='https://via.placeholder.com/300x450' alt="" />
-                    <p>Title (Year)</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus massa diam, laoreet ac euismod eget, faucibus semper ligula. Praesent sed lacus ac tortor euismod varius. Vivamus sed diam libero. Duis.</p>
-                    <p>Runtime:</p>
-                    <p>Director:</p>
-                    <p>Stars:</p>
-                  </div>
-                }
-              </>
-            }
+    <div className='container'>
+      {
+        isLoading ? <div class="loader loader--style3" title="2"><svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="40px" height="40px" viewBox="0 0 50 50" xmlSpace="preserve"><path fill="#000" d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z"><animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" dur="0.6s" repeatCount="indefinite"/></path></svg></div> :
+        <div>
+          <div className='modes'>
+            <p className={`mode ${mode === 'lyrics' && 'active-mode'}`} onClick={() => setMode('lyrics')}>Lyrics</p>
+            <p className={`mode ${mode === 'movies' && 'active-mode'}`} onClick={() => setMode('movies')}>Movies</p>
           </div>
-        }
-      </div>
+          {
+            mode === 'lyrics' ?
+            <div className='search-container'>
+              <h1 className='title'>Search for the lyrics to any song</h1>
+              <input type='text' placeholder='Taylor Swift' value={artist} onChange={e => setArtist(e.target.value)} />
+              <input type='text' placeholder='All Too Well' value={title} onChange={e => setTitle(e.target.value)} />
+              <button className='search' onClick={getLyrics}>Search</button>
+              {
+                lyrics &&
+                <p className='lyrics'>{lyrics}</p>
+              }
+            </div>
+            :
+            <div className='search-container'>
+              <h1 className='title'>Search for a movie</h1>
+              <input type='text' placeholder='Movie Title' />
+              <button className='search'>Search</button>
+              {
+                movie.Title &&
+                <div className='movie-details'>
+                  <img src='https://via.placeholder.com/300x450' alt="" />
+                  <p className='movie-title'>Title (Year)</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus massa diam, laoreet ac euismod eget, faucibus semper ligula. Praesent sed lacus ac tortor euismod varius. Vivamus sed diam libero. Duis.</p>
+                  <p>Runtime:</p>
+                  <p>Director:</p>
+                  <p>Stars:</p>
+                </div>
+              }
+            </div>
+          }
+        </div>
+      }
     </div>
   )
 }
